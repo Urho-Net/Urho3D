832a0850ec0d8f2b11ecc0523032be088e7ef305
Eli Aloni <elix22@gmail.com>
2023-05-24 00:02:04 +0300

    build script fixes for macos

--- script/build_install_urhonet_macos.sh
+++ script/build_install_urhonet_macos.sh
@@ -1,3 +1,19 @@
+URHONET_HOME_ROOT=$(cat ~/.urhonet_config/urhonethome)
+URHO3D_HOME=$(pwd)
+
+if [ ! -d "$URHONET_HOME_ROOT" ]; then
+    echo  "Urho.Net is not configured , please  run configure.sh (configure.bat on Windows) from the Urho.Net installation folder  "
+    exit -1
+else
+    echo "URHONET_HOME_ROOT=${URHONET_HOME_ROOT}"
+fi
+
 ./script/make_csharp_bindings.sh
 ./script/build_xcode_dotnet_dylib.sh 
-./script/copy_urhonet_libs.sh
+
+mkdir -p ${URHONET_HOME_ROOT}/template/libs/dotnet/urho/desktop
+cp -f ${URHO3D_HOME}/DotNet/UrhoDotNet/desktop/UrhoDotNet.dll ${URHONET_HOME_ROOT}/template/libs/dotnet/urho/desktop
+cp -f ${URHO3D_HOME}/DotNet/UrhoDotNet/desktop/UrhoDotNet.xml ${URHONET_HOME_ROOT}/template/libs/dotnet/urho/desktop
+
+mkdir -p ${URHONET_HOME_ROOT}/template/libs/macos
+cp -f ${URHO3D_HOME}/DotNet/libs/macos/Release/libUrho3D.dylib  ${URHONET_HOME_ROOT}/template/libs/macos
--- script/build_xcode_dotnet_dylib.sh
+++ script/build_xcode_dotnet_dylib.sh
@@ -1,4 +1,5 @@
 
 ./script/cmake_xcode_dotnet_dylib.sh build-xcode-dotnet-dylib
 xcodebuild -project  build-xcode-dotnet-dylib/Urho3D.xcodeproj -scheme Urho3D
+mkdir -p DotNet/libs/macos/Release
 cp -Lf build-xcode-dotnet-dylib/lib/libUrho3D.dylib DotNet/libs/macos/Release
