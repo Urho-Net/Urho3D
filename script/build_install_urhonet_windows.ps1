ccd2fd8c15202a005d642486a233ab04e5227573
Eli <elix22@gmail.com>
2023-05-24 14:51:53 +0300

    update Windows build script , check if building from Urho.Net folder

--- script/build_install_urhonet_windows.ps1
+++ script/build_install_urhonet_windows.ps1
@@ -1,4 +1,13 @@
-$URHO3D_HOME=pwd
+$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+$URHO3D_HOME=$scriptDir+"/../"
+
+$configureFile = $scriptDir+"/../../configure.bat"
+if (Test-Path -Path $configureFile -PathType Leaf) {
+    Write-Host "found configure.bat , calling it!"
+    & $configureFile
+}
+
+
 $URHONET_HOME_ROOT = cat $HOME/.urhonet_config/urhonethome
 if (Test-Path -Path $URHONET_HOME_ROOT) 
 {
@@ -9,7 +18,7 @@ else
     echo  "Urho.Net is not configured , please  run configure.sh (configure.bat on Windows) from the Urho.Net installation folder  "
     exit -1
 }
-
+cd $URHO3D_HOME
 # building the Native part an copy it to the Urho.Net folder
 script/cmake_vs2019_dotnet_dll.bat build-vs2019-dotnet-dll
 cmake --build build-vs2019-dotnet-dll/.  --config Release
@@ -20,7 +29,7 @@ cd $URHO3D_HOME/DotNet/Bindings
 ./build-desktop-bindings.bat
 cd $URHO3D_HOME
 
-# copy the dotnet assembly to the Urho.Net folder
+# # copy the dotnet assembly to the Urho.Net folder
 Copy-Item   $URHO3D_HOME/DotNet/UrhoDotNet/desktop/UrhoDotNet.dll -Destination  $URHONET_HOME_ROOT/template/libs/dotnet/urho/desktop
 Copy-Item   $URHO3D_HOME/DotNet/UrhoDotNet/desktop/UrhoDotNet.xml -Destination  $URHONET_HOME_ROOT/template/libs/dotnet/urho/desktop
 
