dbc3b9a7046980eea7ffa435cefdcc36188ba368
Eli Aloni <elix22@gmail.com>
2023-06-03 09:21:52 +0300

    Github Actions w.i.p.

--- script/install_mono.ps1
+++ script/install_mono.ps1
@@ -2,7 +2,8 @@
 $installPath = "$env:TEMP\MonoInstaller.exe"
 $downloadUrl = "https://download.mono-project.com/archive/6.12.0/windows-installer/mono-6.12.0.182-x64-0.msi"
 Invoke-WebRequest -Uri $downloadUrl -OutFile $installPath
-Start-Process -FilePath $installPath -ArgumentList "/quiet" -Wait
+Start-Process -FilePath "msiexec.exe" -ArgumentList "/i", $installPath, "/quiet" -Wait
+
 
 # Configure Mono Environment
 $monoPath = (Get-Command mono).Source
