b98697488280ca02072dbeef724124813b0e313d
Eli Aloni <elix22@gmail.com>
2023-06-03 08:56:10 +0300

    Github Actions w.i.p.

---
+++ script/install_mono.ps1
@@ -0,0 +1,13 @@
+# Install Mono
+$installPath = "$env:TEMP\MonoInstaller.exe"
+$downloadUrl = "https://download.mono-project.com/archive/6.12.0/windows-installer/mono-6.12.0.122-x86_64-0.msi"
+Invoke-WebRequest -Uri $downloadUrl -OutFile $installPath
+Start-Process -FilePath $installPath -ArgumentList "/quiet" -Wait
+
+# Configure Mono Environment
+$monoPath = (Get-Command mono).Source
+$env:PATH += ";$monoPath"
+$env:FrameworkPathOverride = "$monoPath\lib\mono\4.5"
+
+# Display Installation Directory
+Write-Host "Mono installed at: $monoPath"
