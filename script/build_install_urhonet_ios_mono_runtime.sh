b0f96c1a0e58828de2456ee4c92f7e34c1d2aa52
Eli Aloni <elix22@gmail.com>
2023-11-27 12:35:46 +0200

    .NET 8 iOS w.i.p

--- script/build_install_urhonet_ios_mono_runtime.sh
+++ script/build_install_urhonet_ios_mono_runtime.sh
@@ -39,15 +39,19 @@ else
     cd runtime
 fi
 
-git checkout v6.0.21
+# Official .NET 8 release
+git checkout v8.0.0
 
 sed -i "" "s*--minimize**g" "eng/native/functions.cmake"
 ./build.sh mono+libs  -os iOS -arch arm64  -c Release
 
 cp -rf artifacts/bin/mono/iOS.arm64.Release/*.a  ${URHONET_HOME_ROOT}/template/libs/ios/
-cp -rf artifacts/bin/native/net6.0-iOS-Release-arm64/*.a ${URHONET_HOME_ROOT}/template/libs/ios/
-cp -rf artifacts/bin/runtime/net6.0-iOS-Release-arm64/*.dll ${URHONET_HOME_ROOT}/template/libs/dotnet/bcl/ios/
+cp -rf artifacts/bin/native/net8.0-iOS-Release-arm64/*.a ${URHONET_HOME_ROOT}/template/libs/ios/
+cp -rf artifacts/bin/runtime/net8.0-iOS-Release-arm64/*.dll ${URHONET_HOME_ROOT}/template/libs/dotnet/bcl/ios/
 cp -f artifacts/bin/mono/iOS.arm64.Release/System.Private.CoreLib.dll ${URHONET_HOME_ROOT}/template/libs/dotnet/bcl/ios/System.Private.CoreLib.dll
 cp -f artifacts/bin/mono/iOS.arm64.Release/cross/ios-arm64/mono-aot-cross  ${URHONET_HOME_ROOT}/tools/aotcompiler/ios/macos/ios-arm64/aarch64-apple-darwin-mono-sgen
+
+# TBD ELI , For now deleting since it is failing during iOS AOT compilation.
+rm -f ${URHONET_HOME_ROOT}/template/libs/dotnet/bcl/ios/System.Security.Cryptography.ProtectedData.dll 
 
 cd ${URHONET_HOME_ROOT}
