d77a618847e32ad23b7aa9c97c0b861e8ee306e8
Eli Aloni <aloni.eli@gmail.com>
2024-04-10 20:50:49 +0300

    .NET8.x NativeAOT support  W.I.P

---
+++ script/build_xcode_dotnet_metal_dylib.sh
@@ -0,0 +1,5 @@
+
+./script/cmake_xcode_dotnet_metal_dylib.sh build-xcode-dotnet-metal-dylib
+xcodebuild -project  build-xcode-dotnet-metal-dylib/Urho3D.xcodeproj -scheme Urho3D
+mkdir -p DotNet/libs/macos/metal/Release
+cp -Lf build-xcode-dotnet-metal-dylib/lib/libUrho3D.dylib DotNet/libs/macos/metal/Release
