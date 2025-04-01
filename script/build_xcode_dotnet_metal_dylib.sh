3301b7a392f48a490e2d282dd6d224f455525ffb
Eli Aloni <aloni.eli@gmail.com>
2025-04-01 12:32:20 +0300

    build osx binary in release mode

--- script/build_xcode_dotnet_metal_dylib.sh
+++ script/build_xcode_dotnet_metal_dylib.sh
@@ -1,5 +1,5 @@
 
 ./script/cmake_xcode_dotnet_metal_dylib.sh build-xcode-dotnet-metal-dylib
-xcodebuild -project  build-xcode-dotnet-metal-dylib/Urho3D.xcodeproj -scheme Urho3D
+xcodebuild -project  build-xcode-dotnet-metal-dylib/Urho3D.xcodeproj -scheme Urho3D -configuration Release
 mkdir -p DotNet/libs/macos/Release
 cp -Lf build-xcode-dotnet-metal-dylib/lib/libUrho3D.dylib DotNet/libs/macos/Release
