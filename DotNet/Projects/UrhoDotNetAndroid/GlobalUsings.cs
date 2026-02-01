30e3b5fc60d31bc5a21acae4884660c71a7dcdd5
Eli Aloni <aloni.eli@gmail.com>
2026-02-01 10:13:27 +0200

    .net10.0 w.i.p

---
+++ DotNet/Projects/UrhoDotNetAndroid/GlobalUsings.cs
@@ -0,0 +1,4 @@
+// Global using directives to resolve ambiguous references
+global using File = Urho.IO.File;
+global using  ErrorEventArgs  = Urho.Json.Serialization.ErrorEventArgs;
+global using FileMode=Urho.IO.FileMode;
