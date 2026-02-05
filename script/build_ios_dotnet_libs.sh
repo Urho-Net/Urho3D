8bd93a27b8f30172c6ab6c782d74065fdf4f8895
Eli Aloni <aloni.eli@gmail.com>
2026-02-05 14:18:51 +0200

    .github/workflows/build-urho3d-libraries.yml , fixed more errors

--- script/build_ios_dotnet_libs.sh
+++ script/build_ios_dotnet_libs.sh
@@ -2,9 +2,11 @@
 ./script/cmake_ios_dotnet_lib_metal_shared.sh build-ios-dotnet-lib-metal-shared
 
 xcodebuild -project  build-ios-dotnet-lib-metal-shared/Urho3D.xcodeproj -target Urho3D -configuration Release
+mkdir -p DotNet/libs/iphone/Release-iphoneos/
 cp -rf build-ios-dotnet-lib-metal-shared/lib/Urho3D.framework DotNet/libs/iphone/Release-iphoneos/
 cp -f ./script/Info.plist DotNet/libs/iphone/Release-iphoneos/Urho3D.framework
 
 xcodebuild -project  build-ios-dotnet-lib-metal-shared/Urho3D.xcodeproj -target Urho3D -configuration Debug
+mkdir -p DotNet/libs/iphone/Debug-iphoneos/
 cp -rf build-ios-dotnet-lib-metal-shared/lib/Urho3D.framework DotNet/libs/iphone/Debug-iphoneos/
 cp -f ./script/Info.plist DotNet/libs/iphone/Debug-iphoneos/Urho3D.framework
