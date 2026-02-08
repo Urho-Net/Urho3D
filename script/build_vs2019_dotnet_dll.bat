f7539a8fdb45328356934103bdae2cd352924282
Eli Aloni <aloni.eli@gmail.com>
2026-02-08 23:33:22 +0200

    fix window build

--- script/build_vs2019_dotnet_dll.bat
+++ script/build_vs2019_dotnet_dll.bat
@@ -3,13 +3,13 @@ call script/cmake_vs2019_dotnet_dll.bat build-vs2019-dotnet-dll
 echo Building Release configuration...
 cmake --build build-vs2019-dotnet-dll/.  --config Release
 if not exist DotNet\libs\windows\Release mkdir DotNet\libs\windows\Release
-xcopy build-vs2019-dotnet-dll\bin\Release\Urho3D.dll DotNet\libs\windows\Release\ /y
+xcopy build-vs2019-dotnet-dll\bin\Urho3D.dll DotNet\libs\windows\Release\ /y
 echo   OK Windows Release: DotNet\libs\windows\Release\Urho3D.dll
 
 echo Building Debug configuration...
 cmake --build build-vs2019-dotnet-dll/.  --config Debug
 if not exist DotNet\libs\windows\Debug mkdir DotNet\libs\windows\Debug
-xcopy build-vs2019-dotnet-dll\bin\Debug\Urho3D.dll DotNet\libs\windows\Debug\ /y
+xcopy build-vs2019-dotnet-dll\bin\Urho3D_d.dll DotNet\libs\windows\Debug\Urho3D.dll /y
 echo   OK Windows Debug:   DotNet\libs\windows\Debug\Urho3D.dll
 
 echo.
