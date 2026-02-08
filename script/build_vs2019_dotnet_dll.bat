4812f05909e87ec27614a6dd477492655a15ae66
Eli Aloni <aloni.eli@gmail.com>
2026-02-09 00:41:31 +0200

    fix window build + fix libraries combine

--- script/build_vs2019_dotnet_dll.bat
+++ script/build_vs2019_dotnet_dll.bat
@@ -9,8 +9,14 @@ echo   OK Windows Release: DotNet\libs\windows\Release\Urho3D.dll
 echo Building Debug configuration...
 cmake --build build-vs2019-dotnet-dll/.  --config Debug
 if not exist DotNet\libs\windows\Debug mkdir DotNet\libs\windows\Debug
-xcopy build-vs2019-dotnet-dll\bin\Urho3D_d.dll DotNet\libs\windows\Debug\Urho3D.dll /y
-echo   OK Windows Debug:   DotNet\libs\windows\Debug\Urho3D.dll
+xcopy build-vs2019-dotnet-dll\bin\Urho3D_d.dll DotNet\libs\windows\Debug\ /y
+if exist DotNet\libs\windows\Debug\Urho3D_d.dll (
+    ren DotNet\libs\windows\Debug\Urho3D_d.dll Urho3D.dll
+    echo   OK Windows Debug:   DotNet\libs\windows\Debug\Urho3D.dll
+) else (
+    echo   ERROR: Failed to copy Debug DLL
+    exit /b 1
+)
 
 echo.
 echo Build complete:
