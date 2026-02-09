8720121aa189175e3ea6113e5fd5862eb8512c8b
Eli Aloni <aloni.eli@gmail.com>
2026-02-09 08:50:59 +0200

    Add architecture detection steps to the Windows and Linux jobs and use the detected architecture in the upload paths

--- script/build_vs2019_dotnet_editor_dll.bat
+++ script/build_vs2019_dotnet_editor_dll.bat
@@ -1,18 +1,26 @@
 call script/cmake_vs2019_dotnet_editor_dll.bat build-vs2019-dotnet-editor-dll
 
+REM Detect host architecture
+set HOST_ARCH=x64
+if "%PROCESSOR_ARCHITECTURE%"=="AMD64" set HOST_ARCH=x64
+if "%PROCESSOR_ARCHITECTURE%"=="ARM64" set HOST_ARCH=arm64
+if "%PROCESSOR_ARCHITECTURE%"=="x86" set HOST_ARCH=x86
+
+echo Building for Windows %HOST_ARCH%...
+
 echo Building Release configuration...
 cmake --build build-vs2019-dotnet-editor-dll/.  --config Release
-if not exist DotNet\libs\editor\windows\Release mkdir DotNet\libs\editor\windows\Release
-xcopy build-vs2019-dotnet-editor-dll\bin\Urho3D.dll DotNet\libs\editor\windows\Release\ /y
-echo   OK Windows Release: DotNet\libs\editor\windows\Release\Urho3D.dll
+if not exist DotNet\libs\editor\windows\%HOST_ARCH%\Release mkdir DotNet\libs\editor\windows\%HOST_ARCH%\Release
+xcopy build-vs2019-dotnet-editor-dll\bin\Urho3D.dll DotNet\libs\editor\windows\%HOST_ARCH%\Release\ /y
+echo   OK Windows Release: DotNet\libs\editor\windows\%HOST_ARCH%\Release\Urho3D.dll
 
 echo Building Debug configuration...
 cmake --build build-vs2019-dotnet-editor-dll/.  --config Debug
-if not exist DotNet\libs\editor\windows\Debug mkdir DotNet\libs\editor\windows\Debug
-xcopy build-vs2019-dotnet-editor-dll\bin\Urho3D_d.dll DotNet\libs\editor\windows\Debug\ /y
-if exist DotNet\libs\editor\windows\Debug\Urho3D_d.dll (
-    ren DotNet\libs\editor\windows\Debug\Urho3D_d.dll Urho3D.dll
-    echo   OK Windows Debug:   DotNet\libs\editor\windows\Debug\Urho3D.dll
+if not exist DotNet\libs\editor\windows\%HOST_ARCH%\Debug mkdir DotNet\libs\editor\windows\%HOST_ARCH%\Debug
+xcopy build-vs2019-dotnet-editor-dll\bin\Urho3D_d.dll DotNet\libs\editor\windows\%HOST_ARCH%\Debug\ /y
+if exist DotNet\libs\editor\windows\%HOST_ARCH%\Debug\Urho3D_d.dll (
+    ren DotNet\libs\editor\windows\%HOST_ARCH%\Debug\Urho3D_d.dll Urho3D.dll
+    echo   OK Windows Debug:   DotNet\libs\editor\windows\%HOST_ARCH%\Debug\Urho3D.dll
 ) else (
     echo   ERROR: Failed to copy Debug DLL
     exit /b 1
@@ -20,5 +28,5 @@ if exist DotNet\libs\editor\windows\Debug\Urho3D_d.dll (
 
 echo.
 echo Build complete:
-echo   OK Windows Release: DotNet\libs\editor\windows\Release\Urho3D.dll
-echo   OK Windows Debug:   DotNet\libs\editor\windows\Debug\Urho3D.dll
+echo   OK Windows Release: DotNet\libs\editor\windows\%HOST_ARCH%\Release\Urho3D.dll
+echo   OK Windows Debug:   DotNet\libs\editor\windows\%HOST_ARCH%\Debug\Urho3D.dll
